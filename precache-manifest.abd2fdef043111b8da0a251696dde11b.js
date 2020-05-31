3691a455f75af7a3be6f9778a953ffe410d78054
Mehak Zehra <mehak.rizvi.786@gmail.com>
2020-05-31 15:46:26 -0700

    Updates

---
+++ precache-manifest.abd2fdef043111b8da0a251696dde11b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c27bd88e480e26aba86ab949196d85f",
+    "url": "/tutor-ops/index.html"
+  },
+  {
+    "revision": "8c0596282b8b1983b80d",
+    "url": "/tutor-ops/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f92790f2fff0f32a458",
+    "url": "/tutor-ops/static/js/2.766d7742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutor-ops/static/js/2.766d7742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0596282b8b1983b80d",
+    "url": "/tutor-ops/static/js/main.4500c8a1.chunk.js"
+  },
+  {
+    "revision": "8c6363067e75dc9542db",
+    "url": "/tutor-ops/static/js/runtime-main.9738d54f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tutor-ops/static/media/logo.5d5d9eef.svg"
+  }
+]);
