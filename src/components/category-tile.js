a30aef7b9e002773fe46662e46c44c15aa3c9f2a
mrizvi94 <mehak.rizvi.786@gmail.com>
2020-06-28 19:31:20 -0700

    updated copy to clip board button

--- src/components/category-tile.js
+++ src/components/category-tile.js
@@ -2,25 +2,22 @@ import React, { useState } from 'react';
 import undo from '../images/icons8-undo-alt-32.png';
 
 function CategoryTally(props) {
-    const [counter, setcounter] = useState(0);
     const handleClick = () => {
-        setcounter(counter + 1)
-        props.updateTotal()
+        props.setcounter(props.counter + 1)
     }
     return (
         <div className="col-sm-2 category-tally" >
             <div className="image-info">
                 <img className="undo-icon" src={undo} onClick={() => {
-                    if (counter > 0) {
-                        setcounter(counter - 1)
-                        props.decrementTotal();
+                    if (props.counter > 0) {
+                        props.setcounter(props.counter - 1)
                     }
                 }
                 } />
             </div>
 
             <div className="text-center">
-                <p className="counter_label">{counter}</p>
+                <p className="counter_label">{props.counter}</p>
             </div>
             <div className="text-center">
                 <button className="btn_1" onClick={handleClick}>{props.category}</button>
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,12 +2,34 @@ import React, { useState } from 'react';
 import CategoryTally from './category-tile';
 
 function Dashboard() {
-    const [total, setTotal] = useState(0);
-    const incrementTotal = () => {
-        setTotal(total + 1)
-    }
-    const decrementTotal = () => {
-        setTotal(total - 1)
+    const [copied, setcopied] = useState("");
+    const [approve, setApprove] = useState(0);
+    const [reject, setReject] = useState(0);
+    const [recollect, setRecollect] = useState(0);
+    const [finalReview, setFinalReview] = useState(0);
+    const [noCHAccount, setnoCHAccount] = useState(0);
+
+    const copyToClipboard = () => {
+        const el = document.createElement('textarea');
+        el.value =
+            `*_TutorOps_:*\n` +
+            `*APPROVED* = ${approve}\n` +
+            `*REJECTED* = ${reject}\n` +
+            `*RECOLLECT* = ${recollect}\n` +
+            `*FINAL REVIEW* = ${finalReview}\n` +
+            `*NO CH ACCOUNT* = ${noCHAccount}\n` +
+            `_____________________________________\n` +
+            `*TOTAL = ${approve + reject + finalReview + recollect + noCHAccount}*`
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+
+        // update the copied status and remove after 5 seconds
+        setcopied("Stats have been successsfuly copied!")
+        setTimeout(() => {
+            setcopied("")
+        }, 5000);
     }
     return (
         <div className="container">
@@ -20,18 +42,20 @@ function Dashboard() {
                 <hr />
             </div>
             <div className="row " >
-                <CategoryTally category="Approved" updateTotal={incrementTotal} decrementTotal={decrementTotal} />
-                <CategoryTally category="Rejected" updateTotal={incrementTotal} decrementTotal={decrementTotal} />
-                <CategoryTally category="Recollection" updateTotal={incrementTotal} decrementTotal={decrementTotal} />
-                <CategoryTally category="Final Review" updateTotal={incrementTotal} decrementTotal={decrementTotal} />
-                <CategoryTally category="No CH/Category" updateTotal={incrementTotal} decrementTotal={decrementTotal} />
+                <CategoryTally category="Approved" counter={approve} setcounter={setApprove} />
+                <CategoryTally category="Rejected" counter={reject} setcounter={setReject} />
+                <CategoryTally category="Recollection" counter={recollect} setcounter={setRecollect} />
+                <CategoryTally category="Final Review" counter={finalReview} setcounter={setFinalReview} />
+                <CategoryTally category="No CH/Category" counter={noCHAccount} setcounter={setnoCHAccount} />
             </div>
             <div className="row">
                 <hr />
             </div>
 
             <div className=" row total-category">
-                <p className="total-label">total : {total}</p>
+                <p className="total-label">total : {approve + reject + finalReview + recollect + noCHAccount}</p>
+                <button className="clipboard-btn" onClick={copyToClipboard}>Copy to Clipboard</button>
+                <p className="copied-status">{copied}</p>
             </div>
         </div>
     )
